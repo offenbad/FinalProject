7026d2199bdfa5d5e456dda08d7e47e7e3f63c29
Dayna Offenbacher <doffenbacher@gmail.com>
2015-09-30 14:57:37 -0400

    buttons working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
+/////////////COLLECTION PAGE JS//////////////
+
+$( ".js-collection__nav--color" ).on('click',function() {
+	$( ".collection--first" ).remove();
+	$( ".collection--second--color" ).addClass( "collection--second--showing" );
+  });
+
+$( ".js-collection__nav--medium" ).on('click',function() {
+	$( ".collection--first" ).remove();
+	$( ".collection--second--medium" ).addClass( "collection--second--showing" );
+});
+
+
+
+
 
 
 // var item = {
 // 	number: 
-// 	title: 
+// 	title:
+//  creator: 
 // 	department:
 // 	period: 
 // 	year_created: 
@@ -15,13 +31,14 @@
 // }
 
 var item004 = {
-	number: "1979-91-553",
+	number: "1979-91-004",
 	title: "toad",
+	creator: "",
 	department: "Amphibians, small",
 	period: "Art Deco",
-	year_created: 1927,
+	year_created: "",
 	year_acquired: 1972,
-	medium: "carbon",
+	medium: "carbon based life form",
 	provenance: "forrest",
 	creditline: "Gift of Henry Clay Frick",
 	colors: ["#bdb76b", "#eee8aa", "#f5f5dc"],
@@ -31,6 +48,7 @@ var item004 = {
 $(".item__image").append("<img src="+item004.image+">");
 $(".item__number").append("<p>ACCESSION NUMBER: "+item004.number+"</p>");
 $(".item__title").append("<p>TITLE: "+item004.title+"</p>");
+$(".item__creator").append("<p>CREATOR:"+item004.creator+"</p>");
 $(".item__department").append("<p>DEPARTMENT: "+item004.department+"</p>");
 $(".item__period").append("<p>PERIOD: "+item004.period+"</p>");
 $(".item__year_created").append("<p>YEAR CREATED: "+item004.year_created+"</p>");
