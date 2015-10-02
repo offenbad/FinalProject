4ce5de5e2a17a47db416c3251266dc4b0a5ca4b0
Dayna Offenbacher <doffenbacher@gmail.com>
2015-10-02 11:37:45 -0400

    that isotope's operational

--- js/main.js
+++ js/main.js
@@ -1,89 +1,103 @@
-///////////ISOTOPE JS?????????
 
-$( function() {
-  // init Isotope
-  var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-  });
-  
-  // bind filter button click
-  $('.filters-button-group').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-  });
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
-  });
-  
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
 });
 
+// filter items
+$grid.isotope({ filter: '.flora' });
 
+$grid.isotope({ filter: '.fauna' });
 
+$grid.isotope({ filter: '.ephemera' });
 
-/////////////COLLECTION PAGE JS//////////////
+$grid.isotope({ filter: '.indoors' });
 
-$( ".js-collection__nav--color" ).on('click',function() {
-	$( ".collection--first" ).remove();
-	$( ".collection--second--color" ).addClass( "collection--second--showing" );
-  });
+$grid.isotope({ filter: '.outdoors' });
 
-$( ".js-collection__nav--medium" ).on('click',function() {
-	$( ".collection--first" ).remove();
-	$( ".collection--second--medium" ).addClass( "collection--second--showing" );
+$grid.isotope({ filter: '.art' });
+
+// show all items
+$grid.isotope({ filter: '*' });
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
 
 
 
 
 
+// /////////////COLLECTION PAGE JS//////////////
 
-// var item = {
-// 	number: 
-// 	title:
-//  creator: 
-// 	department:
-// 	period: 
-// 	year_created: 
-// 	year_acquired: 
-// 	medium: 
-// 	provenance:
-// 	creditline:
-// 	colors:
-// 	image: 
-// }
+// $( ".js-collection__nav--color" ).on('click',function() {
+// 	$( ".collection--first" ).remove();
+// 	$( ".collection--second--color" ).addClass( "collection--second--showing" );
+//   });
 
-var item004 = {
-	number: "1979-91-004",
-	title: "toad",
-	creator: "",
-	department: "Amphibians, small",
-	period: "Art Deco",
-	year_created: "",
-	year_acquired: 1972,
-	medium: "carbon based life form",
-	provenance: "forrest",
-	creditline: "Gift of Henry Clay Frick",
-	colors: ["#bdb76b", "#eee8aa", "#f5f5dc"],
-	image: "objects/004.jpg",
-}
+// $( ".js-collection__nav--medium" ).on('click',function() {
+// 	$( ".collection--first" ).remove();
+// 	$( ".collection--second--medium" ).addClass( "collection--second--showing" );
+// });
 
-$(".item__image").append("<img src="+item004.image+">");
-$(".item__number").append("<p>ACCESSION NUMBER: "+item004.number+"</p>");
-$(".item__title").append("<p>TITLE: "+item004.title+"</p>");
-$(".item__creator").append("<p>CREATOR:"+item004.creator+"</p>");
-$(".item__department").append("<p>DEPARTMENT: "+item004.department+"</p>");
-$(".item__period").append("<p>PERIOD: "+item004.period+"</p>");
-$(".item__year_created").append("<p>YEAR CREATED: "+item004.year_created+"</p>");
-$(".item__year_acquired").append("<p>YEAR ACQUIRED: "+item004.year_acquired+"</p>");
-$(".item__medium").append("<p>MEDIUM: "+item004.medium+"</p>");
-$(".item__provenance").append("<p>PROVENANCE: "+item004.provenance+"</p>");
-$(".item__creditline").append("<p>"+item004.creditline+"</p>");
-$(".item__colors").append("<p>COLOURS: "+item004.colors+"</p>");
+
+
+
+
+
+// // var item = {
+// // 	number: 
+// // 	title:
+// //  creator: 
+// // 	department:
+// // 	period: 
+// // 	year_created: 
+// // 	year_acquired: 
+// // 	medium: 
+// // 	provenance:
+// // 	creditline:
+// // 	colors:
+// // 	image: 
+// // }
+
+// var items = {
+// 		item004 : {
+// 			number: "1979-91-004",
+// 			title: "toad",
+// 			creator: "",
+// 			department: "Amphibians, small",
+// 			period: "Art Deco",
+// 			year_created: "",
+// 			year_acquired: 1972,
+// 			medium: "carbon based life form",
+// 			provenance: "forrest",
+// 			creditline: "Gift of Henry Clay Frick",
+// 			colors: ["#bdb76b", "#eee8aa", "#f5f5dc"],
+// 			image: "objects/004.jpg"
+// 		},
+
+// 		item005 : {
+
+// 		},
+
+// 		item006 : {
+
+// 		},
+
+
+// 	};
+
+// $(".item__image").append("<img src="+item004.image+">");
+// $(".item__number").append("<p>ACCESSION NUMBER: "+item004.number+"</p>");
+// $(".item__title").append("<p>TITLE: "+item004.title+"</p>");
+// $(".item__creator").append("<p>CREATOR:"+item004.creator+"</p>");
+// $(".item__department").append("<p>DEPARTMENT: "+item004.department+"</p>");
+// $(".item__period").append("<p>PERIOD: "+item004.period+"</p>");
+// $(".item__year_created").append("<p>YEAR CREATED: "+item004.year_created+"</p>");
+// $(".item__year_acquired").append("<p>YEAR ACQUIRED: "+item004.year_acquired+"</p>");
+// $(".item__medium").append("<p>MEDIUM: "+item004.medium+"</p>");
+// $(".item__provenance").append("<p>PROVENANCE: "+item004.provenance+"</p>");
+// $(".item__creditline").append("<p>"+item004.creditline+"</p>");
+// $(".item__colors").append("<p>COLOURS: "+item004.colors+"</p>");
