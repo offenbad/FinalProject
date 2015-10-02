e8e67f753e5dcc8fc030dddf729ca67e08d7db19
Dayna Offenbacher <doffenbacher@gmail.com>
2015-10-01 20:24:41 -0400

    working?

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+///////////ISOTOPE JS?????????
+
+$( function() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
+  
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+});
+
+
+
+
 /////////////COLLECTION PAGE JS//////////////
 
 $( ".js-collection__nav--color" ).on('click',function() {
