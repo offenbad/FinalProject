afbc23f6f6930ddf8bb49a5ce6c68eb956a84a05
Dayna Offenbacher <doffenbacher@gmail.com>
2015-10-03 21:04:07 -0400

    more items

--- js/main.js
+++ js/main.js
@@ -28,8 +28,6 @@ $('.filter-button-group').on( 'click', 'button', function() {
 
 
 
-
-
 // /////////////COLLECTION PAGE JS//////////////
 
 // $( ".js-collection__nav--color" ).on('click',function() {
@@ -47,45 +45,141 @@ $('.filter-button-group').on( 'click', 'button', function() {
 
 
 
-// // var item = {
-// // 	number: 
-// // 	title:
-// //  creator: 
-// // 	department:
-// // 	period: 
-// // 	year_created: 
-// // 	year_acquired: 
-// // 	medium: 
-// // 	provenance:
-// // 	creditline:
-// // 	colors:
-// // 	image: 
-// // }
+// var item = {
+// 	accession_number: 
+// 	title:
+//  	creator: 
+// 	department:
+// 	period: 
+//  	medium: 
+// 	year_created: 
+// 	year_acquired: 
+// 	provenance:
+// 	creditline:
+// 	colors:
+// 	image: 
+// }
 
 // var items = {
+// 		item001 : {
+// 			accession_number: "1999-91-001",
+// 			title: "moth",
+// 			creator: "god",
+// 			department: "Lepeodoptera",
+// 			period: "Art Nouveau",
+// 			medium: "fauna",
+// 			year_created: 1999,
+// 			year_acquired: 1999,
+// 			provenance: "outdoors",
+// 			creditline: null,
+// 			colors: ["light-faun", "warm-faun", "cool-stone", "warm-stone", "almost-black"],
+// 			image: "objects/001.jpg"
+// 		},
+
+// 		item002 : {
+// 			accession_number: "1972-387-002",
+// 			title: "lichen",
+// 			creator: "god",
+// 			department: "Fungi?",
+// 			period: "Art Deco",
+// 			medium: "flora",
+// 			year_created: 1933,
+// 			year_acquired: 1972,
+// 			provenance: "outdoors",
+// 			creditline: "Gift of Henry Clay Frick",
+// 			colors: ["sea-green", "bluey-sonicsilver"],
+// 			image: "objects/002.jpg"
+// 		},
+
+// 		item003 : {
+// 			accession_number: "2015-37-003",
+// 			title: "plant",
+// 			creator: "god",
+// 			department: "Houseplants, potential",
+// 			period: "Art Brut",
+// 			medium: "flora",
+// 			year_created: 2014,
+// 			year_acquired: 2015,
+// 			provenance: "outdoors",
+// 			creditline: null,
+// 			colors: ["plant-green"],
+// 			image: "objects/003.jpg"
+// 		},
+
 // 		item004 : {
-// 			number: "1979-91-004",
+// 			accession_number: "1972-91-004",
 // 			title: "toad",
-// 			creator: "",
+// 			creator: "god",
 // 			department: "Amphibians, small",
 // 			period: "Art Deco",
-// 			year_created: "",
+// 			medium: "fauna",
+// 			year_created: 1937,
 // 			year_acquired: 1972,
-// 			medium: "carbon based life form",
-// 			provenance: "forrest",
+// 			provenance: "outdoors",
 // 			creditline: "Gift of Henry Clay Frick",
-// 			colors: ["#bdb76b", "#eee8aa", "#f5f5dc"],
+// 			colors: ["warm-faun", "warm-stone", "goldenrod"],
 // 			image: "objects/004.jpg"
 // 		},
 
 // 		item005 : {
-
+// 			accession_number: "1187-87-005",
+// 			title: "waterdroplets on plateglass",
+// 		 	creator: "god, man",
+// 			department: "Ephemera",
+// 			period: "Contemporary",
+// 		 	medium: "water, time",
+// 			year_created: "2005",
+// 			year_acquired: "2010",
+// 			provenance: "indoors",
+// 			creditline: "gift of the artist",
+// 			colors: null,
+// 			image: "objects/005.jpg"
 // 		},
 
 // 		item006 : {
-
+// 			accession_number: "1972-87-006",
+// 			title: "sunset in rearview mirror",
+// 		 	creator: "god, man",
+// 			department: "Ephemera",
+// 			period: "Contemporary",
+// 		 	medium: "light, physics",
+// 			year_created: null,
+// 			year_acquired: "1972",
+// 			provenance: "outdoors",
+// 			creditline: "private donor",
+// 			colors: null,
+// 			image: "objects/006.jpg"
 // 		},
 
+// 		item007 : {
+// 			accession_number: "2011-22-007",
+// 			title: "Leonard Lovecraft",
+// 		 	creator: "god",
+// 			department: "Birds",
+// 			period: "Mezozoic",
+// 		 	medium: "fauna",
+// 			year_created: "2009",
+// 			year_acquired: "2011",
+// 			provenance: "indoors",
+// 			creditline: "On loan from K. F. L. Offenbacher",
+// 			colors: ["light-faun", "raw-umber", "sepia", "really-almost-black"],
+// 			image: "objects/007.jpg"
+// 		},
+
+// 		item008 : {
+// 			accession_number: 
+// 			title:
+// 		 	creator: 
+// 			department:
+// 			period: 
+// 		 	medium: 
+// 			year_created: 
+// 			year_acquired: 
+// 			provenance:
+// 			creditline:
+// 			colors:
+// 			image: 
+// 		},
 
 // 	};
 
