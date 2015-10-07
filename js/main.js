94169d5c3bd83c4d40d9db9e15bd1997a489fb25
Dayna Offenbacher <doffenbacher@gmail.com>
2015-10-07 15:48:31 -0400

    few items added, some metadata edits

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ var items = [
 			accession_number: "1999-91-001",
 			title: "moth",
 			creator: "god",
-			department: "Lepeodoptera",
+			department: "Lepeodoptera, common",
 			period: "Art Nouveau",
 			medium: "fauna",
 			year_created: 1999,
 			year_acquired: 1999,
 			provenance: "outdoors",
 			creditline: null,
-			colors: ["light-faun", "warm-faun", "cool-stone", "warm-stone", "almost-black"],
+			colors: ["light-faun", "warm-faun", "cool-stone", "warm-stone"],
 			image: "objects/001.jpg"
 		},
 
@@ -50,7 +50,7 @@ var items = [
 			title: "toad",
 			creator: "god",
 			department: "Amphibians, small",
-			period: "Art Deco",
+			period: "Neoclassical",
 			medium: "fauna",
 			year_created: 1937,
 			year_acquired: 1972,
@@ -61,10 +61,10 @@ var items = [
 		},
 
 		{
-			accession_number: "1187-87-005",
+			accession_number: "2010-87-005",
 			title: "waterdroplets on plateglass",
 		 	creator: "god, man",
-			department: "Ephemera",
+			department: "Ephemera, water event",
 			period: "Contemporary",
 		 	medium: "ephemera",
 			year_created: "2005",
@@ -82,10 +82,10 @@ var items = [
 			department: "Ephemera, light event",
 			period: "Contemporary",
 		 	medium: "ephemera",
-			year_created: null,
+			year_created: "1954",
 			year_acquired: "1972",
 			provenance: "outdoors",
-			creditline: "Private donor",
+			creditline: null,
 			colors: [],
 			image: "objects/006.jpg"
 		},
@@ -101,7 +101,7 @@ var items = [
 			year_acquired: "2011",
 			provenance: "indoors",
 			creditline: "On loan from K. F. L. Offenbacher",
-			colors: ["light-faun", "raw-umber", "sepia", "really-almost-black"],
+			colors: ["almost-black", "sepia", "really-almost-black"],
 			image: "objects/007.jpg"
 		},
 
@@ -116,7 +116,7 @@ var items = [
 			year_acquired: "2011",
 			provenance: "indoors",
 			creditline: "On loan from K. F. L. Offenbacher",
-			colors: ["light-faun", "raw-umber", "sepia", "really-almost-black"],
+			colors: ["raw-umber", "sepia", "really-almost-black"],
 			image: "objects/008.jpg"
 		},
 
@@ -146,19 +146,19 @@ var items = [
 			year_acquired: 2014,
 			provenance: "outdoors",
 			creditline: null,
-			colors: ["warm-stone", "warm-faun", "goldenrod", "almost-black"],
+			colors: ["warm-stone", "goldenrod"],
 			image: "objects/010.jpg"
 		},
 
 		{
-			accession_number: "1970-00-011",
+			accession_number: "2015-00-011",
 			title: "caterpillar in stilllife",
-		 	creator: "man",
-			department: "Drawings and prints",
-			period: "unknown",
+		 	creator: "Jean-Baptiste Monnoyer.",
+			department: "Drawings & prints",
+			period: "Baroque",
 		 	medium: "art",
-			year_created: "unknown",
-			year_acquired: 1970,
+			year_created: "ca. 1680",
+			year_acquired: 2015,
 			provenance: "indoors",
 			creditline: "On loan from Cooper Hewitt",
 			colors: ["light-faun", "realy-almost-black"],
@@ -166,16 +166,16 @@ var items = [
 		},
 
 		{
-			accession_number: "1987-00-011",
+			accession_number: "2015-00-012",
 			title: "Woman in Black Hat",
 		 	creator: "Bil Donovan",
-			department: "Drawings and prints",
+			department: "Drawings & prints",
 			period: "Contemporary",
 		 	medium: "art",
-			year_created: 1982,
-			year_acquired: 1987,
+			year_created: "unknown",
+			year_acquired: 2015,
 			provenance: "indoors",
-			creditline: "Gift of the artist",
+			creditline: "On loan from the artist",
 			colors: ["light-faun", "sonic-silver", "bluey-sonic-silver", "realy-almost-black", "good-brick"],
 			image: "objects/012.jpg"
 		},
@@ -197,7 +197,7 @@ var items = [
 
 		{
 			accession_number: "2015-01-014",
-			title: "house",
+			title: "house; Richmond, VA",
 		 	creator: "man",
 			department: "Architecture, Edward Hopper lighting",
 			period: "unknown",
@@ -221,7 +221,7 @@ var items = [
 			year_acquired: 2015,
 			provenance: "outdoors",
 			creditline: "Memory on loan from Devon Danz",
-			colors: ["goldenrod"],
+			colors: ["goldenrod", "warm-seagreen"],
 			image: "objects/015.jpg"
 		},
 
@@ -244,7 +244,7 @@ var items = [
 			accession_number: "2015-03-017",
 			title: "shade under pier; Wilmington, NC",
 		 	creator: "man, god",
-			department: "Ephemera, architectural",
+			department: "Ephemera, light event",
 			period: "Contemporary",
 		 	medium: "ephemera",
 			year_created: 2015,
@@ -259,7 +259,7 @@ var items = [
 			accession_number: "2015-03-018",
 			title: "chicken on stoop",
 		 	creator: "god",
-			department: "Birds",
+			department: "Birds, ephemera",
 			period: "Cretaceous",
 		 	medium: "fauna",
 			year_created: "unknown",
@@ -274,7 +274,7 @@ var items = [
 			accession_number: "2015-88-019",
 			title: "paint on fingers",
 		 	creator: "man",
-			department: "Ephemera",
+			department: "Ephemera, spills",
 			period: "Contemporary",
 		 	medium: "ephemera",
 			year_created: 2015,
@@ -296,22 +296,22 @@ var items = [
 			year_acquired: 2014,
 			provenance: "indoors",
 			creditline: "Private donor",
-			colors: ["sea-green", "plant-green", "sepia"],
+			colors: ["warm-seagreen", "plant-green", "sepia"],
 			image: "objects/020.jpg"
 		},
 
 		{
-			accession_number: "1966-88-021",
-			title: "unknown",
+			accession_number: "2014-88-021",
+			title: "The Lone Tenement",
 		 	creator: "George Bellows",
 			department: "Painting",
-			period: "unknown",
+			period: "Ashcan school",
 		 	medium: "art",
-			year_created: "unknown",
-			year_acquired: 1966,
+			year_created: 1909,
+			year_acquired: 2014,
 			provenance: "indoors",
 			creditline: "On loan from National Gallery",
-			colors: ["sonic-silver", "bluey-sonicsilver", "cool-stone", "warm-stone", "warm-faun", "peaty-tan", "warm-seagreen", "sea-green", "plant-green", "outer-space", "almost-black", "really-almost-black", "good-brick", "fire-brick", "goldenrod", "raw-umber", "sepia"],
+			colors: ["sonic-silver", "bluey-sonicsilver", "cool-stone", "warm-stone", "warm-faun", "sea-green", "outer-space", "almost-black", "really-almost-black", "good-brick", "fire-brick", "goldenrod", "raw-umber", "sepia"],
 			image: "objects/021.jpg"
 		},
 
@@ -326,7 +326,7 @@ var items = [
 			year_acquired: 2015,
 			provenance: "indoors",
 			creditline: "Permenant loan fron Virginia Museum of Fine Arts",
-			colors: ["warm-faun", "goldenrod", "raw-umber", "sepia", "really-almost-black"],
+			colors: [ "goldenrod", "raw-umber", "sepia", "really-almost-black"],
 			image: "objects/022.jpg"
 		},
 
@@ -446,7 +446,7 @@ var items = [
 			year_acquired: 1933,
 			provenance: "outdoors",
 			creditline: null,
-			colors: ["good-brick", "fire-brick", "raw-umber"],
+			colors: ["good-brick", "fire-brick", "raw-umber", "outer-space"],
 			image: "objects/030.jpg"
 		},
 
@@ -476,7 +476,7 @@ var items = [
 			year_acquired: 1978,
 			provenance: "indoors",
 			creditline: "On loan from Cooper Hewitt",
-			colors: ["light-faun", "cool-stone", "goldenrod", "really-almost-black"],
+			colors: ["goldenrod", "really-almost-black"],
 			image: "objects/032.jpg"
 		},
 
@@ -536,7 +536,7 @@ var items = [
 			year_acquired: 2015,
 			provenance: "indoors",
 			creditline: "Memory on loan from Devon Danz",
-			colors: ["light-faun", "warm-faun", "sea-green", "sepia"],
+			colors: ["warm-faun", "sea-green", "sepia"],
 			image: "objects/036.jpg"
 		},
 
@@ -562,27 +562,162 @@ var items = [
 			department: "Painting, mural",
 			period: "Beaux Arts",
 			medium: "art",
-			year_created: 1922,
+			year_created: 1925,
 			year_acquired: 1931,
 			provenance: "indoors",
-			creditline: "Stolen from Boston Museum of Fine Arts",
+			creditline: null,
 			colors: [ "light-faun", "sonic-silver", "bluey-sonicsilver", "cool-stone", "warm-faun", "warm-stone", "peaty-tan", "outer-space", "almost-black" ],
 			image: "objects/038.jpg"
 		},
 
 		{
 			accession_number: "2015-00-039",
-			title: "unknown",
+			title: "Paul Helleu",
 			creator: "John Singer Sargent",
 			department: "Drawings & prints",
 			period: "Beaux Arts",
 			medium: "art",
+			year_created: "early 1880s",
+			year_acquired: 2015,
+			provenance: "indoors",
+			creditline: "On loan from Metropolitan Museum of Art",
+			colors: [ "light-faun", "cool-stone", "warm-faun", "warm-stone", "really-almost-black" ],
+			image: "objects/039.jpg"
+		},
+
+		{
+			accession_number: "2015-00-040",
+			title: "two dresses",
+			creator: "Karl Lagerfeld",
+			department: "Costume, fanciful",
+			period: "Contemporary",
+			medium: "art",
 			year_created: "unknown",
 			year_acquired: 2015,
 			provenance: "indoors",
-			creditline: "Stolen from Metropolitan Museum of Art",
-			colors: [ "light-faun", "cool-stone", "warm-faun", "warm-stone", "really-almost-black" ],
-			image: "objects/039.jpg"
+			creditline: null,
+			colors: [ "good-brick", "fire-brick", "goldenrod" ],
+			image: "objects/040.jpg"
+		},
+
+		{
+			accession_number: "2015-00-041",
+			title: "metal screws in reused timber",
+			creator: "man, god",
+			department: "Flooring, rather good",
+			period: "Contemporary",
+			medium: "art",
+			year_created: 2014,
+			year_acquired: 2015,
+			provenance: "indoors",
+			creditline: "Borrowed from the Whitney",
+			colors: [ "light-faun", "warm-faun", "outer-space" ],
+			image: "objects/041.jpg"
+		},
+
+		{
+			accession_number: "2015-00-42",
+			title: "beach, pre-picnic",
+			creator: "man, god",
+			department: "Leisure, summer",
+			period: "Contemporary",
+			medium: "flora",
+			year_created: 2015,
+			year_acquired: 2015,
+			provenance: "outdoors",
+			creditline: "Memory on loan from Devon Danz",
+			colors: [ "light-faun", "warm-faun", "almost-black", "plant-green"],
+			image: "objects/042.jpg"
+		},
+
+		{
+			accession_number: "2014-00-43",
+			title: "my favourite building on 5th avenue",
+			creator: "man",
+			department: "Architecture, envy",
+			period: "Beaux Arts",
+			medium: "art",
+			year_created: "unknown",
+			year_acquired: 2014,
+			provenance: "outdoors",
+			creditline: "Gift of the artist",
+			colors: [ "good-brick"],
+			image: "objects/043.jpg"
+		},
+
+		{
+			accession_number: "2015-00-44",
+			title: "inkstain",
+			creator: "man",
+			department: "Ephemera, spills",
+			period: "Fin de Siecle",
+			medium: "ephemera",
+			year_created: 2015,
+			year_acquired: 2015,
+			provenance: "outdoors",
+			creditline: "Gift of the artist",
+			colors: [ "really-almost-black"],
+			image: "objects/044.jpg"
+		},
+
+		{
+			accession_number: "2015-00-45",
+			title: "water on stones",
+			creator: "god",
+			department: "Picnic spots, summer",
+			period: "Pre-cambrian",
+			medium: "ephemera",
+			year_created: "unknown",
+			year_acquired: 2015,
+			provenance: "outdoors",
+			creditline: "",
+			colors: [ "warm-stone", "cool-stone"],
+			image: "objects/045.jpg"
+		},
+
+		{
+			accession_number: "2014-00-46",
+			title: "Sevres cup and saucer",
+			creator: "man",
+			department: "Tablewear, nostalgia",
+			period: "unknown",
+			medium: "art",
+			year_created: "ca. 1780",
+			year_acquired: 2014,
+			provenance: "indoors",
+			creditline: "On loan from Cooper Hewitt, suggested by Denton Welch",
+			colors: [ "goldenrod"],
+			image: "objects/046.jpg"
+		},
+
+		{
+			accession_number: "2014-82-47",
+			title: "Poor Artist's Cupboard",
+			creator: "Charles Bird King",
+			department: "Painting",
+			period: "unknown",
+			medium: "art",
+			year_created: 1815,
+			year_acquired: 2014,
+			provenance: "indoors",
+			creditline: "On loan from National Gallery",
+			colors: [ "goldenrod", "warm-faun", "outer-space", "really-almost-black", "fire-brick", "almost-black"],
+			image: "objects/047.jpg"
+		},
+
+		{
+			accession_number: "2013-82-48",
+			title: "Portrait of the Artist in an Improbable Hat",
+			creator: "Igor Gnedo",
+			department: "Drawings & prints",
+			period: "Contemporary",
+			medium: "art",
+			year_created: 2013,
+			year_acquired: 2013,
+			provenance: "indoors",
+			creditline: "Captured on Instagram",
+			colors: [ "really-almost-black"],
+			image: "objects/048.jpg"
 		},
 
 	];
